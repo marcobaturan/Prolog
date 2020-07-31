44ff13f2c6e0fd676d1832a5392e255ca568f8a3
Marco Baturan <marco.baturan@gmail.com>
2020-07-31 16:00:49 +0200

    Add files via upload

--- lesson15.py
+++ lesson15.py
@@ -22,24 +22,7 @@ try:
 except NameError:
     pass
 
-
-def main():
-    prolog = Prolog()  # Call class
-    prolog.consult("coins.pl") # Call consult
-    sg.theme('DarkAmber')   # Add a touch of color
-    # All the stuff inside your window.
-    layout = [[sg.Text("How many coins (default: 100)? "), sg.InputText()],
-              [sg.Text("What should be the total (default: 500)? "), sg.InputText()],
-              [sg.Button('Coins!')] ]
-    # Create the Window
-    window = sg.Window('Python-Swi-Prolog: Coins', layout)
-    # Event Loop to process "events" and get the "values" of the inputs
-    while True:
-        event, values = window.read()
-        if event == sg.WIN_CLOSED or event == 'Coins!': # if user closes window or clicks cancel
-            break
-    window.close()
-    # Variables
+def consult(values):
     count = int(values[0] or 100)
     total = int(values[1] or 500)
     coins = Functor("coins", 3)
@@ -54,8 +37,32 @@ def main():
         for c, v in s:
             print("%dx%d" % (c,v), end=" ")
         print()
-        i += 1
+    i += 1
     q.closeQuery()
+
+def main():
+    prolog = Prolog()  # Call class
+    prolog.consult("coins.pl") # Call consult
+    sg.theme('DarkAmber')   # Add a touch of color
+    # All the stuff inside your window.
+    layout = [[sg.Text("How many coins (default: 100)? "), sg.InputText()],
+              [sg.Text("What should be the total (default: 500)? "), sg.InputText()],
+              [sg.Button('Coins!'),sg.Button('Close!')] ]
+    # Create the Window
+    window = sg.Window('Python-Swi-Prolog: Coins', layout)
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED:
+            break
+        elif event == 'Coins!': # if user closes window or clicks cancel
+            consult(values=values)
+        elif event == 'Close!':
+            break
+        else:
+            pass
+    window.close()
+
 
 
 if __name__ == "__main__":
